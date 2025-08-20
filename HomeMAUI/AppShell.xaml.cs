7162b29a40298963c2f222318335c0782ddb1282
Giurea Renato <1447560+rgiurea@users.noreply.github.com>
2025-08-20 17:34:19 +0100

    Move toolbar item into Shell.ToolbarItems

--- HomeMAUI/AppShell.xaml.cs
+++ HomeMAUI/AppShell.xaml.cs
@@ -2,8 +2,13 @@
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+        public AppShell()
+        {
+                InitializeComponent();
+        }
+
+        private void OnMenuClicked(object sender, EventArgs e)
+        {
+                Shell.Current.FlyoutIsPresented = true;
+        }
 }
